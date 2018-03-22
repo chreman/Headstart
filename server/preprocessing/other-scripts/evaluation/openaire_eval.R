ae97da56e6080cdcd7f04830f63e1732d9cb08ee
Christopher Kittel <web@christopherkittel.eu>
2018-03-22 02:14:16 +0100

    first try at eval metrics: missing dois

--- server/preprocessing/other-scripts/evaluation/openaire_eval.R
+++ server/preprocessing/other-scripts/evaluation/openaire_eval.R
@@ -32,6 +32,14 @@ produce_dataset <- function(org, project){
   })
 }
 
+get_metrics <- function(query, params, output) {
+  tmp <- c(query=query, unlist(params))
+  tmp <- data.frame(t(tmp))
+  tmp$missing_dois <- sum(output$doi == "")
+  tmp$n_papers <- length(output$id)
+  eval_metrics <<- rbind.fill(eval_metrics, tmp)
+}
+
 export_project_vis <- function(query, params){
   input_data = get_papers(query, params)
   if (nrow(input_data$metadata) > 2){
@@ -45,6 +53,7 @@ export_project_vis <- function(query, params){
     output$cluster_labels <- vapply(output$cluster_labels, paste, collapse = ", ", character(1L))
     output$readers <- ""
     output$file_hash <- ""
+    get_metrics(output)
     write.table(output, file=paste0("../../../../examples/local_files/openaire/",
                                     params$org, "_", query, ".csv"), sep=",", row.names=FALSE)
   }
@@ -53,11 +62,14 @@ export_project_vis <- function(query, params){
 # run workflow
 organizations <- read.csv("openaire.csv")
 total_projects <- data.frame()
+eval_metrics <- data.frame()
 for (organization in organizations$org_openaire){
   org_projects <- unique(roa_projects(org=organization))
   org_projects <- org_projects[which(org_projects$funding_level_0 == 'FP7'),]
   org_projects$organization <- organization
   by(org_projects, 1:nrow(org_projects), function(project) {produce_dataset(organization, project)})
   total_projects <- rbind.fill(total_projects, org_projects)
+  print(eval_metrics)
 }
-write.table(total_projects, file="openaire_projects.csv")
+write.table(total_projects, file="openaire_projects.csv", sep=",", row.names=FALSE)
+write.table(eval_metrics, file="openaire_eval_metrics.csv", sep=",", row.names=FALSE)
